2f75d27f3be0d1942a8777d4d66c2c2b0985f11b
Kevin Marchais <kevinmarchais@gmail.com>
2024-07-11 19:07:33 +0200

    update test

--- tests/test_imports.py
+++ tests/test_imports.py
@@ -1,9 +1,15 @@
+from pathlib import Path
+
 from blender_vtk_importer_exporter.importer import sort_files
 
+
 def test_sort_files() -> None:
-    files = ["file-2.vtk", "file-5.vtk", "file-3.vtk", "file-1.vtk", "file-4.vtk"]
+    files = [
+        Path("file-2.vtk"),
+        Path("file-5.vtk"),
+        Path("file-3.vtk"),
+        Path("file-1.vtk"),
+        Path("file-4.vtk"),
+    ]
     sorted_files = sort_files(files)
     assert sorted_files == sorted(files)
-
-if __name__ == "__main__":
-    test_sort_files()
