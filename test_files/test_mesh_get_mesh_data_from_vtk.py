9afa84b909a6f76d290857da014247ce82b0624c
saubert-dev <aubertstef@free.fr>
2026-07-29 17:46:21 +0200

    Unitary tests of mesh.get_mesh_data_from_vtk

--- test_files/test_mesh_get_mesh_data_from_vtk.py
+++ test_files/test_mesh_get_mesh_data_from_vtk.py
@@ -1,36 +1,47 @@
 # Unitary tests of mesh.get_mesh_data_from_vtk()
-# HowTo: In Blender/Scripting workspace, use 'Text > Open' to read this script, then 'Text > Run Script' to execute it
+# HowTo: In Blender/Scripting workspace, use "Text > Open" to read this script, then "Text > Run Script" to execute it
 
 import importlib
 import sys
+import inspect
 import traceback
+import numpy as np
 import pyvista as pv
 
 # Reload modified python scripts during development and testing
 #   see: https://blender.stackexchange.com/questions/28504/blender-ignores-changes-to-python-scripts
 #   see: https://docs.python.org/3/library/importlib.html#importlib.reload
-current_package_prefix = 'blender-vtk-importer-exporter-main'
+current_package_prefix = "blender-vtk-importer-exporter-main"
 for name, module in sys.modules.copy().items():
     if name.startswith(current_package_prefix):
         print(f"Reloading {name}")
         importlib.reload(module)
-mesh = importlib.import_module(current_package_prefix+'.mesh')
+mesh = importlib.import_module(current_package_prefix+".mesh")
 
-print()
-
-# UnstructuredGrid / single triangle without attributes
-print('TEST: UnstructuredGrid / single triangle without attributes')
-points = [[0.0, 0.0, 0.0],
-          [1.0, 0.0, 0.0],
-          [0.0, 1.0, 0.0]]
-cells = [[3, 0, 1, 2]]
-celltypes = [[pv.CellType.TRIANGLE]]
-vtk_data = pv.UnstructuredGrid(cells, celltypes, points)
-# print(vtk_data)
-try:
-    vertices, edges, faces = mesh.get_mesh_data_from_vtk(vtk_data)
-except:
-    print('..... FAILED')
-    print(traceback.format_exc())
-else:
-    print('..... PASS')
+# Wrapper
+def test_get_mesh_data_from_vtk(vtk_data):
+    print("TEST: " + inspect.currentframe().f_back.f_code.co_name)
+    try:
+        vertices, edges, faces = mesh.get_mesh_data_from_vtk(vtk_data)
+    except:
+        print("..... FAILED")
+        print(traceback.format_exc())
+    else:
+        print("..... PASS")
+        
+# Unstructured grid with a single triangle
+def test_UnstructuredGrid_one_triangle():
+    points = np.asarray([[0.0, 0.0, 0.0],
+                         [1.0, 0.0, 0.0],
+                         [0.0, 1.0, 0.0]])
+    cells = np.asarray([3, 0, 1, 2])
+    celltypes = [pv.CellType.TRIANGLE]
+    vtk_data = pv.UnstructuredGrid(cells, celltypes, points)
+    test_get_mesh_data_from_vtk(vtk_data)
+        
+def main():
+    print()
+    test_UnstructuredGrid_one_triangle()
+    
+if __name__ == "__main__":
+    main()
