d4e24796bb0d349c16bfaf9fc9013e0d5d350ebf
Stéphane AUBERT <stephane.aubert67@gmail.com>
2026-08-05 15:13:54 +0200

    Function unique_mesh_name() moved in a new file (utilities.py)

--- tests/test_mesh_vtk_to_mesh.py
+++ tests/test_mesh_vtk_to_mesh.py
@@ -7,21 +7,10 @@ import pyvista as pv
 
 import bpy
 
+from utilities import *
+
 
 m_mesh = importlib.import_module("blender-vtk-importer-exporter-main.mesh")
-
-
-def unique_mesh_name():
-    prefix_val = "pytest_mesh_"
-    prefix_len = len(prefix_val)
-    highest_suffix = 0
-    for mesh_name in bpy.data.meshes.keys():
-        if mesh_name.startswith(prefix_val):
-            highest_suffix = max(
-                highest_suffix,
-                float(mesh_name[prefix_len:]) # float() is required to handle e.g. 2.001
-            )
-    return f"{prefix_val}{int(highest_suffix)+1}"
 
 
 class TestClass:
---
+++ tests/utilities.py
@@ -0,0 +1,17 @@
+# Functions shared by unit tests
+
+import bpy
+
+
+def unique_mesh_name():
+    prefix_val = "pytest_mesh_"
+    prefix_len = len(prefix_val)
+    highest_suffix = 0
+    for mesh_name in bpy.data.meshes.keys():
+        if mesh_name.startswith(prefix_val):
+            highest_suffix = max(
+                highest_suffix,
+                float(mesh_name[prefix_len:]) # float() is required to handle e.g. 2.001
+            )
+    return f"{prefix_val}{int(highest_suffix)+1}"
+
